ef46688f5a93e070e1f60cbc87b791dc62bcd9df
Maxime Mahdavian <maxime.mahdavian@ssc-spc.gc.ca>
2025-01-09 10:12:25 -0500

    Modification to backup logic to support jump servers

--- backup.tf
+++ backup.tf
@@ -10,23 +10,25 @@ locals {
 
 # Get the RSV from the target sub
 data "azurerm_recovery_services_vault" "rsv" {
+  count = try(var.linux_VM.jump_server, false) == true ? 0 : 1
   name                = local.rsv-name
   resource_group_name = var.resource_groups["Backups"].name 
 }
 
 # Get the desired backup policy from the RSV
 data "azurerm_backup_policy_vm" "backup_policy" {
+  count = try(var.linux_VM.jump_server, false) == true ? 0 : 1
   name                = local.backup-policy-name
-  recovery_vault_name = data.azurerm_recovery_services_vault.rsv.name
-  resource_group_name = data.azurerm_recovery_services_vault.rsv.resource_group_name
+  recovery_vault_name = data.azurerm_recovery_services_vault.rsv[0].name
+  resource_group_name = data.azurerm_recovery_services_vault.rsv[0].resource_group_name
 }
 
 resource "azurerm_backup_protected_vm" "backup_vm" {
   count               = try(var.linux_VM.disable_backup, false) == false ? 1 : 0
-  resource_group_name = data.azurerm_recovery_services_vault.rsv.resource_group_name
-  recovery_vault_name = data.azurerm_recovery_services_vault.rsv.name
+  resource_group_name = try(var.linux_VM.jump_server, false) == true ? var.resource_group["Backups"].name : data.azurerm_recovery_services_vault.rsv.resource_group_name
+  recovery_vault_name = try(var.linux_VM.jump_server, false) == true ? local.rsv-name : data.azurerm_recovery_services_vault.rsv[0].name
   source_vm_id        = azurerm_linux_virtual_machine.vm.id
-  backup_policy_id    = data.azurerm_backup_policy_vm.backup_policy.id
+  backup_policy_id    = try(var.linux_VM.jump_server, false) == true ? var.linux_VM.backup_policy : data.azurerm_backup_policy_vm.backup_policy.id
 
   exclude_disk_luns = try(var.linux_VM.backup.exclude_disk_luns, null)
   include_disk_luns = try(var.linux_VM.backup.include_disk_luns, null)
